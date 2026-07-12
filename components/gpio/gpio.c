10ee473a2f4619ed7be02e323ab92075556ecf41
MURLETHARAN <murletharan4@gmail.com>
2026-07-12 12:43:51 +0530

    updated the functions to be more low-level

--- components/gpio/gpio.c
+++ components/gpio/gpio.c
@@ -2,51 +2,44 @@
 
 #include "gpio.h"
 
-#include "soc/gpio_reg.h"
-#include "soc/gpio_struct.h"
-#include "soc/io_mux_reg.h"
-#include "soc/soc.h"
+// GPIO peripheral base is 0x3FF44000 on ESP32 (pins 0-31)
+#define GPIO_OUT_REG           (*(volatile uint32_t *)0x3FF44004)
+#define GPIO_OUT_W1TS_REG      (*(volatile uint32_t *)0x3FF44008)
+#define GPIO_OUT_W1TC_REG      (*(volatile uint32_t *)0x3FF4400C)
+#define GPIO_ENABLE_W1TS_REG   (*(volatile uint32_t *)0x3FF44024)
+#define GPIO_ENABLE_W1TC_REG   (*(volatile uint32_t *)0x3FF44028)
+#define GPIO_IN_REG            (*(volatile uint32_t *)0x3FF4403C)
 
-void gpio_init(uint8_t pin,  gpio_mode_t mode){
-    if(mode==GPIO_MODE_OUTPUT){
-        //Enable output
-        GPIO.enable_w1ts=(1<<pin);//if pin=4 then 00000000 --> 00001000
-    }
-    else{
-        //Disable output
-        GPIO.enable_w1tc = (1<<pin);//pin=4 00001000 --> 00000000
-    }
-}
-
-void gpio_write(uint8_t pin, uint8_t level){
-    if (level){
-        GPIO.out_w1ts=(1<<pin);// set HIGH
-    }
-    else{
-        GPIO.out_w1tc=(1<<pin);//set LOW
+void gpio_init(uint8_t pin, gpio_mode_t mode) {
+    if (mode == GPIO_MODE_OUTPUT) {
+        GPIO_ENABLE_W1TS_REG = (1 << pin);
+    } else {
+        GPIO_ENABLE_W1TC_REG = (1 << pin);
     }
 }
 
-void gpio_toggle(uint8_t pin){
-    if(GPIO.out & (1<<pin)){// if non zero pin=4 00001000 --> 00000000
-        GPIO.out_w1tc=(1<<pin);
+void gpio_write(uint8_t pin, uint8_t level) {
+    if (level) {
+        GPIO_OUT_W1TS_REG = (1 << pin);
+    } else {
+        GPIO_OUT_W1TC_REG = (1 << pin);
     }
-    else{
-        GPIO.out_w1ts=(1<<pin); 
+}
+
+void gpio_toggle(uint8_t pin) {
+    if (GPIO_OUT_REG & (1 << pin)) {
+        GPIO_OUT_W1TC_REG = (1 << pin);
+    } else {
+        GPIO_OUT_W1TS_REG = (1 << pin);
     }
 }
 
-uint8_t gpio_read(uint8_t pin){
-    return (GPIO.in >> pin) & 0x1;
+uint8_t gpio_read(uint8_t pin) {
+    return (GPIO_IN_REG >> pin) & 0x1;
 }
 
-void gpio_set(uint8_t pin){
-    gpio_set_level(pin, 1);
-}
-
-void gpio_clear(uint8_t pin){
-    gpio_set_level(pin, 0);
-}
+void gpio_set(uint8_t pin)   { gpio_write(pin, 1); }
+void gpio_clear(uint8_t pin) { gpio_write(pin, 0); }
 
 /*
 #include "gpio.h"
